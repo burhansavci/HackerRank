c6adfe9d4e8e058a2a5282d0e93d49fd229827c9
Burhan SAVCI <burhansavci@gmail.com>
2019-09-13 09:15:37 +0300

    Easy questions added

---
+++ Easy Questions/AppleAndOrange/AppleAndOrange/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleAndOrange
+{
+    class Program
+    {
+        // Complete the countApplesAndOranges function below.
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            Console.Clear();
+            var feltDownApples = new List<int>();
+            for (int i = 0; i < apples.Length; i++)
+            {
+                var feldownApple = apples[i] + a;
+                if (s <= feldownApple && feldownApple <= t)
+                    feltDownApples.Add(feldownApple);
+            }
+            var feltDownOranges = new List<int>();
+            for (int i = 0; i < oranges.Length; i++)
+            {
+                var feltDownOrange = oranges[i] + b;
+                if (s <= feltDownOrange && feltDownOrange <= t)
+                    feltDownOranges.Add(feltDownOrange);
+            }
+            Console.WriteLine(feltDownApples.Count);
+            Console.WriteLine(feltDownOranges.Count);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the start and the end point of Sam's House");
+            string[] st = Console.ReadLine().Split(' ');
+
+            int s = Convert.ToInt32(st[0]);
+
+            int t = Convert.ToInt32(st[1]);
+            Console.WriteLine("Please enter location of apple and orange tree ");
+            string[] ab = Console.ReadLine().Split(' ');
+
+            int a = Convert.ToInt32(ab[0]);
+
+            int b = Convert.ToInt32(ab[1]);
+            Console.WriteLine("Please enter the numbers of apples and oranges");
+            string[] mn = Console.ReadLine().Split(' ');
+
+            int m = Convert.ToInt32(mn[0]);
+
+            int n = Convert.ToInt32(mn[1]);
+            Console.WriteLine("Please enter distances of apples");
+            int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+            ;
+            Console.WriteLine("Please enter distances of oranges");
+            int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+            ;
+            countApplesAndOranges(s, t, a, b, apples, oranges);
+        }
+    }
+}
+
+
---
+++ Easy Questions/Birthday_Cakes/Birthday_Cakes/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday_Cakes
+{
+    class Program
+    {
+        static int birthdayCakeCandles(int[] ar)
+        {
+            var counter = 1;
+            var orderedArr = ar.OrderByDescending(x => x).ToArray();
+            for (int i = 1; i < orderedArr.Length; i++)
+            {
+                if (orderedArr[i] != orderedArr[i -1])
+                {
+                    break;
+                }
+                counter++;
+            }
+            return counter;
+        }
+
+        static void Main(string[] args)
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            
+            int result = birthdayCakeCandles(ar);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+            #region OtherSolutions
+            #region 1
+            //var heights = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            //var max = heights.Max();
+            //var count = heights.Count(it => it == max);
+            //Console.WriteLine(count);
+            #endregion
+            #region 2
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //string[] arr_temp = Console.ReadLine().Split(' ');
+            //int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            //int valueMax = 0;
+            //int value = 0;
+
+            //valueMax = arr[0];
+            //for (int k = 0; k < arr.Length; k++)
+            //{
+            //    if (arr[k] > valueMax)
+            //    {
+            //        valueMax = arr[k];
+            //        value = 1;
+            //    }
+            //    else if (arr[k] == valueMax)
+            //    {
+            //        value++;
+            //    }
+            //}
+            //Console.WriteLine(value.ToString());
+            #endregion
+            #region 3
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //string[] arr_temp = Console.ReadLine().Split(' ');
+            //List<Int32> l1 = Array.ConvertAll(arr_temp, Int32.Parse).ToList<Int32>();
+            //int successCount = 0;
+            //int tallestCandle = l1.Max();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    if (tallestCandle == l1[i])
+            //        successCount++;
+            //}
+            //Console.WriteLine(successCount);
+            #endregion
+            #endregion
+
+        }
+    }
+}
