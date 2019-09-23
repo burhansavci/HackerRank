0e8e3b113b074e7620e05c7462ec6bb383533454
Burhan SAVCI <burhansavci@gmail.com>
2019-09-23 07:17:30 +0300

    ViralAdvertising Question Added

---
+++ Easy Questions/ViralAdvertising/ViralAdvertising/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        static int viralAdvertising(int n)
+        {
+            int d = 5;
+            int l = 0;
+            for (int i = 0; i < n; i++)
+            {
+                l += (d / 2);
+                d = (d / 2) * 3;
+            }
+            return l;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = viralAdvertising(n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
