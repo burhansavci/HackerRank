6222c951e0ae0e7798e05650d90f72329a8bc86c
Burhan SAVCI <burhansavci@gmail.com>
2019-10-05 12:16:57 +0300

    SequenceEquatioan Question Added.

---
+++ Easy Questions/SequenceEquatiton/SequenceEquatiton/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace SequenceEquatiton
+{
+    class Program
+    {
+        static int[] permutationEquation(int[] p)
+        {
+            var table = new Hashtable();
+            var yArr = new int[p.Length];
+            for (int i = 0; i < p.Length; i++)
+            {
+                table[p[i]] = i + 1;
+            }
+            for (int i = 0; i < yArr.Length; i++)
+            {
+                yArr[i] = (int)table[table[i + 1]];
+            }
+            return yArr;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] p = Array.ConvertAll(Console.ReadLine().Split(' '), pTemp => Convert.ToInt32(pTemp));
+
+            int[] result = permutationEquation(p);
+
+            Console.WriteLine(string.Join("\n", result));
+        }
+
+        #region OtherSolutions1
+
+        //public static void Main(String[] args)
+        //{
+
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int[] p_inverse = new int[n + 1];
+        //    for (int x = 1; x <= n; x++)
+        //    {
+        //        int px = Convert.ToInt32(Console.ReadLine());
+        //        p_inverse[px] = x;
+        //    }
+
+        //    for (int x = 1; x <= n; x++)
+        //    {
+        //        int y = p_inverse[p_inverse[x]];
+        //        Console.WriteLine(y);
+        //    }
+        //    Console.ReadLine();
+        //}
+
+        #endregion
+    }
+}
