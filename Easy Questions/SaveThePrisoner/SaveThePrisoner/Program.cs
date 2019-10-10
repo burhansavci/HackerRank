118ad167fd977046b8b40a8423c211c6edd1a012
Burhan SAVCI <burhansavci@gmail.com>
2019-10-10 23:42:55 +0300

    SaveThePrisoner question added.

---
+++ Easy Questions/SaveThePrisoner/SaveThePrisoner/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SaveThePrisoner
+{
+    class Program
+    {
+        static int saveThePrisoner(int n, int m, int s)
+        {
+            if ((m + s - 1) % n == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return (m + s - 1) % n;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] nms = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nms[0]);
+
+                int m = Convert.ToInt32(nms[1]);
+
+                int s = Convert.ToInt32(nms[2]);
+
+                int result = saveThePrisoner(n, m, s);
+
+                Console.WriteLine(result);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
