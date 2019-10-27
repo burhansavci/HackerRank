69e76ba9f4bd5ca673dac8b468747ebf0001830b
Burhan SAVCI <burhansavci@gmail.com>
2019-10-27 10:13:30 +0300

    ExtraLongFactorials question added.

---
+++ Medium Questions/ExtraLongFactorials/ExtraLongFactorials/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static BigInteger extraLongFactorials(int n)
+        {
+            if (n==0||n==1)          
+                return 1;
+            
+            return extraLongFactorials(n - 1) * n;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(extraLongFactorials(n));
+        }
+    }
+}
