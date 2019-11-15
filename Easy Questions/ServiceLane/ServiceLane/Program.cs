5cca05d17c3b3bc97c26748479842fab44324271
Burhan SAVCI <burhansavci@gmail.com>
2019-11-15 20:10:52 +0300

    ServiceLane question added.

---
+++ Easy Questions/ServiceLane/ServiceLane/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ServiceLane
+{
+    class Program
+    {
+        static int[] serviceLane(int[][] cases, int[] widths)
+        {
+            var result = new int[cases.Length];
+            for (int i = 0; i < cases.Length; i++)
+            {
+                int min = int.MaxValue;
+                for (int j = cases[i][0]; j <= cases[i][1]; j++)
+                    if (min > widths[j]) min = widths[j];
+
+                result[i] = min;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string[] nt = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nt[0]);
+
+            int t = Convert.ToInt32(nt[1]);
+
+            int[] widths = Array.ConvertAll(Console.ReadLine().Split(' '), widthTemp => Convert.ToInt32(widthTemp));
+
+
+            int[][] cases = new int[t][];
+
+            for (int i = 0; i < t; i++)
+            {
+                cases[i] = Array.ConvertAll(Console.ReadLine().Split(' '), casesTemp => Convert.ToInt32(casesTemp));
+            }
+
+            int[] result = serviceLane(cases, widths);
+
+            Console.WriteLine(string.Join("\n", result));
+
+        }
+    }
+}
