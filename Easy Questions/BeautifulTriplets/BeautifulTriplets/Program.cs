d64b1ba443bd301c0f4f56a4c9b087ce852e66a7
Burhan SAVCI <burhansavci@gmail.com>
2019-12-01 22:03:36 +0300

    BeautifulTriplets question added.

---
+++ Easy Questions/BeautifulTriplets/BeautifulTriplets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeautifulTriplets
+{
+    class Program
+    {
+        static int beautifulTriplets(int d, int[] arr)
+        {
+            int beautifulTripletCounter = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                if (arr.Contains(arr[i] + d) && arr.Contains(arr[i] + 2 * d))
+                    beautifulTripletCounter++;
+
+            return beautifulTripletCounter;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string[] nd = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nd[0]);
+
+            int d = Convert.ToInt32(nd[1]);
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int result = beautifulTriplets(d, arr);
+
+            Console.WriteLine(result);
+        }
+    }
+
+}
