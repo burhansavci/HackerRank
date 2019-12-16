072cceaf734d1ee33fdfd5e034d1db62ec214503
Burhan SAVCI <burhansavci@gmail.com>
2019-12-16 22:24:11 +0300

    BubbleSort question added.

---
+++ Easy Questions/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void countSwaps(int[] a)
+        {
+            int counter = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Array is sorted in {counter} swaps.");
+            Console.WriteLine($"First Element: {a[0]}");
+            Console.WriteLine($"Last Element: {a[a.Length - 1]}");
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+            countSwaps(a);
+        }
+    }
+}
