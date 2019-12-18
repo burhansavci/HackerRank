7e1013414c222deb18b74e7f28dd5cf27a3bdbf1
Burhan SAVCI <burhansavci@gmail.com>
2019-12-18 22:51:28 +0300

    MakingAnagrams question added

---
+++ Easy Questions/MakingAnagrams/MakingAnagrams/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MakingAnagrams
+{
+    class Program
+    {
+        static int makeAnagram(string a, string b)
+        {
+            var arr = new int[26];
+            foreach (var item in a)            
+                arr[item - 'a']++;
+            
+            foreach (var item in b)           
+                arr[item - 'a']--;
+            
+            return arr.Where(x => x != 0).Sum(x => Math.Abs(x));
+        }
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            string b = Console.ReadLine();
+
+            int res = makeAnagram(a, b);
+
+            Console.WriteLine(res);
+        }
+
+        #region OtherSolutions1
+        //static void Main(String[] args)
+        //{
+        //    string a = Console.ReadLine();
+        //    string b = Console.ReadLine();
+
+        //    Dictionary<char, int> frequencyA = CountChars(a);
+        //    Dictionary<char, int> frequencyB = CountChars(b);
+
+        //    int removalCount = 0;
+
+        //    foreach (var key in frequencyA.Keys)
+        //    {
+        //        removalCount += Math.Abs(frequencyA[key] - frequencyB[key]);
+        //    }
+
+        //    Console.WriteLine(removalCount);
+
+        //}
+
+        //public static Dictionary<char, int> CountChars(string input)
+        //{
+        //    Dictionary<char, int> letterFrequency = new Dictionary<char, int>();
+        //    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        //    foreach (var c in alphabet)
+        //    {
+        //        letterFrequency.Add(c, 0);
+        //    }
+
+        //    foreach (var c in input)
+        //    {
+        //        letterFrequency[c]++;
+        //    }
+        //    return letterFrequency;
+        //}
+        #endregion
+    }
+}
