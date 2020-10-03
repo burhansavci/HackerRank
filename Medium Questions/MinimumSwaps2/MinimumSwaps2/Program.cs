592ec1c11d060b24ff5a037fe27fcee123b65630
Burhan SAVCI <burhansavci@gmail.com>
2020-10-03 21:46:08 +0300

    Add MinumumSwap2 question

---
+++ Medium Questions/MinimumSwaps2/MinimumSwaps2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MinimumSwaps2
+{
+    class Program
+    {
+        static int MinimumSwaps(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != i + 1)
+                {
+                    var temp = arr[arr[i] - 1];
+                    arr[arr[i] - 1] = arr[i];
+                    arr[i] = temp;
+                    i--;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int res = MinimumSwaps(arr);
+
+            Console.WriteLine(res);
+        }
+    }
+}
