012653bcb4051b5d0521761f02217bae08f4aac2
Burhan SAVCI <burhansavci@gmail.com>
2021-02-22 21:58:09 +0300

    Add Alternating Characters question

---
+++ Easy Questions/AlternatingCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AlternatingCharacters
+{
+    class Program
+    {
+        static int AlternatingCharacters(string s)
+        {
+            var deletionCounts = 0;
+            for (var i = 1; i < s.Length; i++)
+                if (s[i - 1] == s[i])
+                    deletionCounts++;
+
+            return deletionCounts;
+        }
+
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = AlternatingCharacters(s);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Easy Questions/AlternatingCharacters/README.md
@@ -0,0 +1,1 @@
+You can find this quesiton [here](https://www.hackerrank.com/challenges/alternating-characters/problem)
