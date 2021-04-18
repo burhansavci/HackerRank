bef2c31a62efcda79000b8968e04bbcfb109e3c8
Burhan SAVCI <burhansavci@gmail.com>
2021-04-18 17:16:56 +0300

    Add Ice Cream Parlor question

---
+++ Medium Questions/IceCreamParlor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IceCreamParlor
+{
+    class Program
+    {
+        static void whatFlavors(int[] cost, int money)
+        {
+            var dict = new Dictionary<int, int>();
+            for (var i = 0; i < cost.Length; i++)
+            {
+                var complement = money - cost[i];
+
+                if (dict.ContainsKey(complement))
+                {
+                    // Console.WriteLine($"{Math.Min(dict[complement], i + 1)} {Math.Max(dict[complement], i + 1)}");
+                    Console.WriteLine($"{dict[complement]} {i + 1}");
+                    break;
+                }
+
+                dict.Add(cost[i], i + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int money = Convert.ToInt32(Console.ReadLine());
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] cost = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+                whatFlavors(cost, money);
+            }
+        }
+    }
+}
