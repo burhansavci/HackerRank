2bd5b457acbca5fd3c2f28d6f97ad302e57945a3
Burhan SAVCI <burhansavci@gmail.com>
2021-05-09 18:46:20 +0300

    Add Special String Again question

---
+++ Medium Questions/SpecialStringAgain/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpecialStringAgain
+{
+    class Program
+    {
+        static long substrCount(string s)
+        {
+            long substrCount = 0;
+
+            var tupleList = new List<(char character, long count)>();
+
+            var currentChar = s[0];
+            long count = 1;
+            for (var i = 1; i < s.Length; i++)
+                if (s[i] == currentChar)
+                    count++;
+                else
+                {
+                    tupleList.Add((currentChar, count));
+                    count = 1;
+                    currentChar = s[i];
+                }
+            
+            //Dont forget to add last sequence
+            tupleList.Add((currentChar, count));
+
+            foreach (var tuple in tupleList)
+                substrCount += tuple.count * (tuple.count + 1) / 2; //combinations of same characters 
+
+            for (var i = 1; i < tupleList.Count - 1; i++)
+                if (tupleList[i - 1].character == tupleList[i + 1].character && tupleList[i].count == 1)
+                    substrCount += Math.Min(tupleList[i - 1].count, tupleList[i + 1].count);
+
+            return substrCount;
+        }
+
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            var s = Console.ReadLine();
+
+            var result = substrCount(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
