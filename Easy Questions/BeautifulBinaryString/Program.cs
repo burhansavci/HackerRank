9c219a6192e460ce1aa59eefcf0d69c45a4dfaf3
Burhan SAVCI <burhansavci@gmail.com>
2021-08-01 19:27:57 +0300

    Add BeautifulBinaryString question

---
+++ Easy Questions/BeautifulBinaryString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BeautifulBinaryString
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            var b = Console.ReadLine();
+
+            var result = beautifulBinaryString(b);
+
+            Console.WriteLine(result);
+        }
+
+        public static int beautifulBinaryString(string b)
+        {
+            var beautifulBinaryString = b.Replace("010", string.Empty);
+            return (b.Length - beautifulBinaryString.Length) / 3;
+        }
+    }
+}
